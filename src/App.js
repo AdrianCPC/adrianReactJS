18a4a2c089a150234f5380c52d1074435fb5c5e4
AdrianPC <adrianchooo18@gmail.com>
2021-11-13 17:08:08 -0500

    component navbar fisrt part finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import './Normalize.css';
 import Coverpage from './components/coverpage/Coverpage';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
+
+  //Logical for changed color when scrolling to down on navbar
+  const [scrollHeight, setScrollHeight] = useState(0);
+  
+  //Control position
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollHeight(position);
+  };
+
+  //Update scroll
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+  }, [scrollHeight]);
+
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar scrollView={scrollHeight}/>
       <Coverpage/>
     </div>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Navbar.css';
-import Adrian from '../../assets/icons/AP.gif'
+import Adrian from '../../assets/icons/AP.png'
 
-const Navbar = () => {
+const Navbar = ({scrollView}) => {
+    //Click on navbar for back top
+    const backTop = () => {
+        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+    }
+
     return (
-        <nav className="navbar">
-            <div className="navbar-logo">
+        <nav className={`navbar ${scrollView > 20 ? 'scrolling' : null}`}>
+            <div className="navbar-logo" onClick={backTop}>
                 <img src={Adrian} alt="adrian-peña-desarrollador-web"/>
             </div>
         </nav>
