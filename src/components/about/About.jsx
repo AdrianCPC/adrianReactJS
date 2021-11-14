ae42363b1d5c665b95c2367d46dd31b5eafab08b
AdrianPC <adrianchooo18@gmail.com>
2021-11-13 20:00:34 -0500

    reduce mp4 for 364mb, created section about

---
+++ src/components/about/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+    return (
+        <div className="about-container">
+            <div className="about-text">
+                <h3>Acerca de mi</h3>
+                <p>
+                    fdfsdfdsgdfgfdhfdhgfhgfhgfhj
+                    sfdsdgfgdfgdfgsdhgfdsf
+                    esagfjsdfgdsfjhdfghdsfgjdsgf
+                </p>
+            </div>
+        </div>
+    )
+};
+
+export default About;
